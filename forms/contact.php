701171f1416ce4eceeb2f99acf9b745bb6c46d57
Shweta Khatra <khatrashweta10314@gmail.com>
2021-11-20 13:52:20 +0100

    emaildebugging

--- forms/contact.php
+++ forms/contact.php
@@ -8,7 +8,6 @@
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'khatrashweta10314@gmail.com';
-  echo "here";die;
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
